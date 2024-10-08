7fd00ba55ac6392a8db73c9a49be5a5274051d0c
Julio Martinez <julio.martinez@iudigital.edu.co>
2024-10-08 22:53:54 +0200

    :construction: cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config()
  app.use(
     cors({
         origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Permite métodos
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD', 'PATCH', 'TRACE', 'CONNECT'], // Permite métodos
         allowedHeaders: ['Content-Type', 'Authorization', 'token'], // Cabeceras permitidas
         credentials: true
     })
